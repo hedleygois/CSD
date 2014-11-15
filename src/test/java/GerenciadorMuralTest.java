7ca93b0565269ad9fac874bb676b761bd3aa3944
brunobc <brunobezerrachaves@gmail.com>
2014-11-15 14:23:22 -0300

    Projeto inicial

---
+++ src/test/java/GerenciadorMuralTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+
+public class GerenciadorMuralTest {
+	
+	@Test
+	public void deveMoverTarefa() {
+		GerenciadorMural gerenciador = new GerenciadorMural();
+		
+		
+	}
+
+}
