4438342aa247ba7df5eaf40b06b60a71f792c375
Hedley Luna <hedleygois@gmail.com>
2014-11-15 14:34:12 -0300

    Inserçao do teste de criaçao de tarefas

--- src/test/java/GerenciadorMuralTest.java
+++ src/test/java/GerenciadorMuralTest.java
@@ -1,7 +1,23 @@
+import static org.junit.Assert.*;
 import models.GerenciadorMural;
+import models.GerenciadorMuralImpl;
+import models.Tarefa;
+import org.junit.Before;
+import org.junit.Test;
 
 public class GerenciadorMuralTest {
 
     private GerenciadorMural mural;
+
+    @Before
+    public void setUp() {
+        mural = new GerenciadorMuralImpl();
+    }
+
+    @Test
+    public void deveCriarUmaTarefaComONomeEspecificado() {
+        Tarefa tarefa = mural.criarTarefa("Teste");
+        assertEquals("Teste", tarefa.getNome());
+    }
 
 }
